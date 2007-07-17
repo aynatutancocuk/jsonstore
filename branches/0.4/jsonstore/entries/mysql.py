d0e3b63812731eb93b597eeace9af8e7820c58b1
ralmeida <ralmeida@16e461c2-281b-0410-9b54-55d16e500c56>
2007-07-17 15:42:27 +0000

    Just need to add regepxs.

--- branches/0.4/jsonstore/entries/mysql.py
+++ branches/0.4/jsonstore/entries/mysql.py
@@ -1,13 +1,14 @@
 import urllib
 import itertools
 import operator
+import threading
+LOCAL = threading.local()
 
 import cjson
 from MySQLdb import connect
 
 
 # TODO: regular expression
-# thread-safe? check docs
 
 
 def split_location(location):
@@ -46,11 +47,16 @@ def flatten(obj, keys=[]):
 class EntryManager(object):
     def __init__(self, location):
         # Create connection.
-        params = split_location(location)
-        self.conn = connect(**params)
+        self.params = split_location(location)
 
         # Create table if it doesn't exist.
         self._create_table()
+
+    @property
+    def conn(self):
+        if not hasattr(LOCAL, "connection"):
+            LOCAL.connection = connect(**self.params)
+        return LOCAL.connection
 
     def _create_table(self):
         curs = self.conn.cursor()
@@ -66,7 +72,7 @@ class EntryManager(object):
                 id INTEGER,
                 position CHAR(255),
                 leaf TEXT,
-                updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);
+                INDEX (position));
         """)
         self.conn.commit()
 
@@ -112,6 +118,7 @@ class EntryManager(object):
         query = ["SELECT id, entry, updated FROM store"]
         if size is not None: query.append("LIMIT %s" % size)
         if offset: query.append("OFFSET %s" % offset)
+        query.append("ORDER BY updated DESC")
         curs.execute(' '.join(query))
 
         entries = []
@@ -171,32 +178,46 @@ class EntryManager(object):
 
         curs = self.conn.cursor()
 
-        # Build search query.
-        query = ["SELECT id, entry, updated FROM store WHERE id IN (SELECT id FROM flat"]
+        # Build search query. We use two separate queries, since MySQL has 
+        # incredibly slow subqueries.
+        # http://mysql2.mirrors-r-us.net/doc/refman/5.0/en/in-subquery-optimization.html
+        # http://blog.ealden.net/article/mysql-50-and-in-subquery
+        query = ["SELECT matches.id id FROM (SELECT id, count(*) AS n FROM flat"]
         if groups: query.append("WHERE")
         condition = []
         params = []
 
-	# Check groups from groupby, they should be joined within
-	# using an OR.
+        # Check groups from groupby, they should be joined within
+        # using an OR.
+        count = 0
         for (key, group) in groups:
             group = list(group)
-            unsed = [params.extend(t) for t in group]
+            unused = [params.extend(t) for t in group]
             subquery = ["(position=%s AND leaf=%s)" for t in group]
             condition.append(' OR '.join(subquery))
+            count += len(unused)
         # Join all conditions with an AND.
-        condition = ') AND ('.join(condition)
-        query.append("(%s)" % condition)
-
-        query.append(")")  # close the subselect
-        if size is not None: query.append("LIMIT %s" % size)
-        if offset: query.append("OFFSET %s" % offset)
-
+        condition = ' OR '.join(condition)
+        query.append(condition)
+        query.append('GROUP BY id) AS matches WHERE matches.n=%d' % count)  # close subselect
         query = ' '.join(query)
+        print query.replace("%s", "'%s'") % tuple(params)
         curs.execute(query, tuple(params))
+        ids = [str(row[0]) for row in curs.fetchall()]
+
+        if ids:
+            query = ["SELECT id, entry, updated FROM store WHERE id IN (%s)" % ', '.join(ids)]
+            if size is not None: query.append("LIMIT %s" % size)
+            if offset: query.append("OFFSET %s" % offset)
+            query.append("ORDER BY updated DESC")
+            query = ' '.join(query)
+            curs.execute(query)
+            results = curs.fetchall()
+        else:
+            results = []
 
         entries = []
-        for id_, entry, updated in curs.fetchall():
+        for id_, entry, updated in results:
             entry = cjson.decode(entry)
             entry['__id__'] = id_
             entry['__updated__'] = updated
