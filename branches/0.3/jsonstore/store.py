106c3903e92a8bd260bb872f8bec5d10e40b8923
ralmeida <ralmeida@16e461c2-281b-0410-9b54-55d16e500c56>
2008-01-12 02:35:37 +0000

    Working fine! :)

--- branches/0.3/jsonstore/store.py
+++ branches/0.3/jsonstore/store.py
@@ -1,214 +1,149 @@
+import re
 from urllib import unquote
 from urlparse import urljoin
-import md5
-import datetime
-import time
+from datetime import datetime
+import sha
 
-from paste import httpexceptions
-from paste.request import parse_dict_querystring, construct_url
-from httpencode import parse_request, get_format
-from simplejson import dumps, loads
+from webob import Request, Response
+from simplejson import loads, dumps, JSONEncoder
 
 from jsonstore.backends import EntryManager
+from jsonstore import rison
+from jsonstore import operators
 
 
-def make_app(global_conf, dsn, number_of_entries=10, **kwargs):
-    """
-    Create a JSON Atom store.
+def make_app(global_conf, dsn='sqlite://test.db', **kwargs):
+    return JSONStore(dsn)
 
-    Configuration should be like this::
 
-        [app:jsonstore]
-        use = egg:jsonstore
-        dsn = driver://user:password@host:port/dbname
-        number_of_entries = 10
-
-    """
-    noe = int(number_of_entries)
-    store = JSONStore(dsn, number_of_entries=noe)
-    return store
+class DatetimeEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat().split('.', 1)[0] + 'Z'
 
 
 class JSONStore(object):
-    """
-    A RESTful store based on JSON.
-
-    """
-    def __init__(self, dsn, number_of_entries=10, responder=None):
+    def __init__(self, dsn):
         self.em = EntryManager(dsn)
-        self.number_of_entries = number_of_entries
-        self.responder = responder or get_format('json').responder
 
     def __call__(self, environ, start_response):
-        query = parse_dict_querystring(environ)
-        method = (query.get('REQUEST_METHOD') or
-                environ['REQUEST_METHOD'])
-        func = getattr(self, '_%s' % method)
-        return func(environ, start_response)
+        req = Request(environ)
+        func = getattr(self, req.method)
+        res = func(req)
+        return res(environ, start_response)
 
-    def _GET(self, environ, start_response):
-        # Unserialize PATH_INFO to a JSON object.
-        path_info = environ.get('PATH_INFO', '/')
+    def GET(self, req):
+        path_info = req.path_info.lstrip('/') or '{}'  # empty search
         path_info = unquote(path_info)
-        path_info = path_info.strip('/') or 'null'  # use null if path is /
-        obj = loads(path_info)
-        
-        # Single entry.
-        if isinstance(obj, (int, long, float, basestring)): 
+        obj = load_entry(path_info)
+
+        jsonp = req.GET.get('jsonp') or req.GET.get('callback')
+        size = req.GET.get('size')
+        offset = req.GET.get('offset')
+
+        if isinstance(obj, (int, long, float, basestring)):
             try:
-                output = self.em.get_entry(obj)
-            except (KeyError, TypeError):
-                raise httpexceptions.HTTPNotFound()  # 404
-
-            output.setdefault('@namespaces', {})
-            output['jsonstore:id'] = output.pop('__id__')
-            output['jsonstore:updated'] = output.pop('__updated__')
-
-        # Collection from listing or search.
+                result = self.em.search({'__id__': obj}, size, offset)[0]
+                items = 1
+            except IndexError:
+                return Response(status='404 Not Found')
         else:
-            query = parse_dict_querystring(environ)
-            size = int(query.get("size", self.number_of_entries))
-            offset = int(query.get("offset", 0))
+            obj = replace_operators(obj)
+            items = self.em.search(obj, count=True)
+            result = self.em.search(obj, size, offset)
+        
+        body = dumps(result, cls=DatetimeEncoder)
+        if jsonp:
+            body = jsonp + '(' + body + ')'
+        etag = '"%s"' % sha.sha(body).hexdigest()
+        if etag in req.if_none_match:
+            return Response(status='304 Not Modified')
 
-            # Return store listing.
-            if obj is None:
-                entries = self.em.get_entries(size+1, offset)
-            # Return a JSON search.
-            else:
-                # Convert external identifiers to internal.
-                if 'jsonstore:id' in obj: 
-                    obj['__id__'] = obj.pop('jsonstore:id')
-                if 'jsonstore:updated' in obj:
-                    obj['__updated__'] = obj.pop('jsonstore:updated')
-                mode = int(query.get("search", 0))
-                entries = self.em.search(obj, mode, size+1, offset)
+        if req.method == 'HEAD': body = ''
 
-            output = {'@namespaces': {}}
-
-            # Check number of entries for a "next" entry.
-            if len(entries) == size+1:
-                entries.pop()  # remove "next" entry
-                qs = query.copy()
-                qs['size'] = str(size)
-                qs['offset'] = str(offset+size)
-                qs = '&'.join(['%s=%s' % p for p in qs.items()])
-                output['jep:next'] = construct_url(environ, querystring=qs)
-            else:
-                output['jep:next'] = None
-
-            # Change jsonstore identifiers to namespaced variables.
-            for entry in entries:
-                entry['jsonstore:id'] = entry.pop('__id__')
-                entry['jsonstore:updated'] = entry.pop('__updated__')
-                if '@namespaces' in entry:
-                    ns = entry.pop('@namespaces')
-                    output['@namespaces'].update(ns)
-                etag = md5.new(dumps(entry)).hexdigest()
-                entry['jep:etag'] = etag
-            
-            baseurl = construct_url(environ,
-                    with_query_string=False, with_path_info=False)
-            output['jep:members'] = [
-                    {'jep:href': '%s/%s' % (baseurl, entry['jsonstore:id']),
-                     'jep:entity': entry}
-                    for entry in entries]
-
-        # Add namespace to object.
-        output['@namespaces']['jsonstore'] = "http://dealmeida.net/projects/jsonstore/"
-        output['@namespaces']['jep'] = "http://bitworking.org/news/JEP"
-
-        # Calculate etag.
-        etag = md5.new(dumps(output)).hexdigest()
-        output['jep:etag'] = etag
-
-        app = self.responder(output,
+        return Response(
+                body=body,
                 content_type='application/json',
-                headers=[('Etag', etag)])
-        return app(environ, start_response)
+                charset='utf8',
+                headerlist=[('X-ITEMS', str(items)), ('etag', etag)])
+    HEAD = GET
 
-    def _HEAD(self, environ, start_response):
-        self._GET(environ, start_response)
-        return []
+    def POST(self, req):
+        entry = load_entry(req.body)
 
-    def _POST(self, environ, start_response):
-        entry = parse_request(environ, output_type='python')
+        result = self.em.create(entry)
+        body = dumps(result, cls=DatetimeEncoder)
+        location = urljoin(req.application_url, result['__id__'])
 
-        # Change __updated__ to a datetime object.
-        if 'jsonstore:updated' in entry:
-            updated = entry.pop('jsonstore:updated')
-            entry['__updated__'] = datetime.datetime(
-                    *(time.strptime(updated, '%Y-%m-%dT%H:%M:%SZ')[0:6]))
-        if 'jsonstore:id' in entry:
-            entry['__id__'] = entry.pop('jsonstore:id')
-
-        # Create the entry.
-        output = self.em.create_entry(entry)
-        output['jsonstore:id'] = output.pop('__id__')
-        output['jsonstore:updated'] = output.pop('__updated__')
-        output.setdefault('@namespaces', {})
-        output['@namespaces']['jsonstore'] = "http://dealmeida.net/projects/jsonstore/"
-        output['@namespaces']['jep'] = "http://bitworking.org/news/JEP"
-
-        # Generate new resource location.
-        store = construct_url(environ, with_query_string=False,
-                with_path_info=False)
-        location = urljoin(store, str(output['jsonstore:id']))
-        app = self.responder(output,
+        return Response(
+                status='201 Created',
+                body=body,
                 content_type='application/json',
-                headers=[('Location', location)])
+                charset='utf8',
+                headerlist=[('Location', location)])
 
-        # Fake start response to return 201 status.
-        def start(status, headers):
-            return start_response("201 Created", headers)
+    def PUT(self, req):
+        entry = load_entry(req.body)
+        if '__id__' not in entry:
+            entry['__id__'] = req.path_info.lstrip('/')
+        elif req.path_info.lstrip('/') != entry['__id__']:
+            return Response(status='409 Conflict')
 
-        return app(environ, start)
+        result = self.em.update(entry)
+        body = dumps(result, cls=DatetimeEncoder)
 
-    def _PUT(self, environ, start_response):
-        entry = parse_request(environ, output_type='python')
+        return Response(
+                body=body,
+                content_type='application/json',
+                charset='utf8')
+        
+    def DELETE(self, req):
+        id_ = req.path_info.lstrip('/')
+        self.em.delete(id_)
 
-        # Change __updated__ to a datetime object.
-        if 'jsonstore:updated' in entry:
-            updated = entry.pop('jsonstore:updated')
-            entry['__updated__'] = datetime.datetime(
-                    *(time.strptime(updated, '%Y-%m-%dT%H:%M:%SZ')[0:6]))
+        return Response(
+                body='null',
+                content_type='application/json',
+                charset='utf8')
 
-        path_info = environ.get('PATH_INFO', '/')
-        path_info = path_info.strip('/')
-        id_ = unquote(path_info)
 
-        if 'jsonstore:id' in entry:
-            entry['__id__'] = entry.pop('jsonstore:id')
-            if id_ != entry['__id__']: raise httpexceptions.HTTPConflict()
+def load_entry(s):
+    try:
+        entry = loads(s)
+    except ValueError:
+        try:
+            entry = rison.loads(s)
+        except rison.ParserException:
+            entry = s
+    return entry
+
+
+def replace_operators(obj):
+    for k, v in obj.items():
+        if isinstance(v, dict):
+            obj[k] = replace_operators(v)
+        elif isinstance(v, list):
+            for i, item in enumerate(v):
+                obj[k][i] = parse_op(item)
         else:
-            entry['__id__'] = id_
+            obj[k] = parse_op(v)
+    return obj
 
-        # Update entry.
-        output = self.em.update_entry(entry)
-        output['jsonstore:id'] = output.pop('__id__')
-        output['jsonstore:updated'] = output.pop('__updated__')
-        output.setdefault('@namespaces', {})
-        output['@namespaces']['jsonstore'] = "http://dealmeida.net/projects/jsonstore/"
-        output['@namespaces']['jep'] = "http://bitworking.org/news/JEP"
 
-        # Calculate etag.
-        etag = md5.new(dumps(output)).hexdigest()
-        output['jep:etag'] = etag
+def parse_op(obj):
+    if not isinstance(obj, basestring):
+        return obj
 
-        app = self.responder(output,
-                content_type='application/json',
-                headers=[('Etag', etag)])
-        return app(environ, start_response)
+    for op in operators.__all__:
+        if obj.startswith(op + '('):
+            operator = getattr(operators, op)
+            args = re.match(op + r'\((.*?)\)', obj).group(1)
+            args = loads('[' + args + ']')
+            return operator(*args)
+    return obj
 
-    def _DELETE(self, environ, start_response):
-        path_info = environ.get('PATH_INFO', '/')
-        path_info = path_info.strip('/')
-        id_ = unquote(path_info)
 
-        self.em.delete_entry(id_)
-
-        app = self.responder(None, content_type='application/json')
-        return app(environ, start_response)
-
-    def close(self):
-        self.em.close()
-
+if __name__ == '__main__':
+    from paste.httpserver import serve
+    app = JSONStore('sqlite://test.db')
+    serve(app, port=8081)
